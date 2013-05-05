5c9f90f91284081d3ec6d7a8c0e6deb7280707c0
Bladeking68 <roborex97@gmail.com>
2013-05-05 17:03:06 +1000

    Update PaleocraftBlocksCreativeTab.java

--- bladeking68/paleocraft/dimension/PaleocraftBlocksCreativeTab.java
+++ bladeking68/paleocraft/dimension/PaleocraftBlocksCreativeTab.java
@@ -2,7 +2,7 @@ package bladeking68.paleocraft.dimension;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import cpw.mods.fml.relauncher.*;
-import fisherman77.zeuscraft.common.Zeuscraft;
+//import fisherman77.zeuscraft.common.Zeuscraft;
 
 
 public class PaleocraftBlocksCreativeTab extends CreativeTabs {
@@ -12,7 +12,7 @@ super(position, tabID); //The constructor for your tab
 @SideOnly(Side.CLIENT)
 public int getTabIconItemIndex() //The item it displays for your tab
 {
-return Item.bone.itemID; //For this we'll use the ruby
+return fisherman77.paleocraft.common.Paleocraft.fossil.itemID; //For this we'll use the ruby
 }
 public String getTranslatedTabLabel()
 {
