d42fb1ada911ff88667caac1b32aab918e344f77
Bladeking68 <roborex97@gmail.com>
2013-06-01 19:56:26 +1100

    Update BiomeGenPaleoplains.java

--- bladeking68/paleocraft/dimension/BiomeGenPaleoplains.java
+++ bladeking68/paleocraft/dimension/BiomeGenPaleoplains.java
@@ -1,20 +1,31 @@
 package bladeking68.paleocraft.dimension;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.SpawnListEntry;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 
 public class BiomeGenPaleoplains extends BiomeGenBase
 {
+       private WorldGenMinable theWorldGenerator;
+
        public BiomeGenPaleoplains(int par1)
        {
              super(par1);
-             this.theBiomeDecorator.treesPerChunk = 0;
-             this.theBiomeDecorator.flowersPerChunk = 0;
-             this.theBiomeDecorator.deadBushPerChunk = 1;
-              this.spawnableMonsterList.clear();
+             this.theBiomeDecorator.treesPerChunk = -999;
+             this.theBiomeDecorator.flowersPerChunk = -999;
+             this.theBiomeDecorator.deadBushPerChunk = -999;
+             this.theBiomeDecorator.grassPerChunk = -999;
+             this.theWorldGenerator = new WorldGenMinable(Block.pumpkin.blockID, -999);
+             this.spawnableMonsterList.clear();
              this.spawnableCreatureList.clear();
              this.spawnableWaterCreatureList.clear();
-             //this.spawnableMonsterList.add(new SpawnListEntry(EntityNAME.class, 1, 4, 4)); //Fisherman just keep coping this for each dino and the numbers are the spawn rate i think so you can edit that if you want
-      
-      
-      }
+          //   this.spawnableMonsterList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityDromaeosaurus.class, 1, 4, 4)); //Fisherman just keep coping this for each dino and the numbers are the spawn rate i think so you can edit that if you want
+             this.spawnableCreatureList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityDromaeosaurus.class, 3, 4, 4));
+             this.spawnableCreatureList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityCitipati.class,  3, 1, 1));
+             this.spawnableCreatureList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityDimorphodon.class,  4, 4, 4));
+             this.spawnableCreatureList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityBaryonyx.class,  1, 1, 1));
+             this.spawnableCreatureList.add(new SpawnListEntry(fisherman77.paleocraft.common.mobs.EntityTroodon.class,  2, 1, 1));
+       }
 }
