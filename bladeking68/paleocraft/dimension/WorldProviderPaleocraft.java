b4d054af972ac49f6172da6f3cf0e0a76c0b6067
Bladeking68 <roborex97@gmail.com>
2013-06-26 21:15:50 +1000

    Update WorldProviderPaleocraft.java

--- bladeking68/paleocraft/dimension/WorldProviderPaleocraft.java
+++ bladeking68/paleocraft/dimension/WorldProviderPaleocraft.java
@@ -1,52 +1,90 @@
-
 package bladeking68.paleocraft.dimension;
 
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.WorldChunkManagerHell;
+import net.minecraft.world.WorldProviderHell;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraftforge.common.DimensionManager;
+
+
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class WorldProviderPaleocraft extends WorldProvider
 {
- public void registerWorldChunkManager()
- {
-  this.worldChunkMgr = new WorldChunkManagerPaleocraft(fisherman77.paleocraft.common.Paleocraft.Paleoplains);
-  this.dimensionId = fisherman77.paleocraft.common.Paleocraft.PaleocraftDimension;
- }
- 
- public String getDimensionName() 
- {
-  return "Paleocraft Dimension";
- }
- 
- public boolean canRespawnHere()
- {
-  return true;
- }
- 
- public String getSaveFolder()
- {
-     return "Paleocraft Dimension";
- }
-
- public boolean renderClouds()
- {
-
-  return true;
-
- }
- 
- public boolean renderStars()
- {
-
-  return true;
-
- }
-
- 
- @Override
- public IChunkProvider createChunkGenerator()
- {
-  return new ChunkProviderPaleocraft(worldObj, worldObj.getSeed(), true);
- }
+public void registerWorldChunkManager()
+{
+/** tells Minecraft to use our new WorldChunkManager **/
+this.worldChunkMgr = new WorldChunkManagerPaleocraft(worldObj.getSeed(), terrainType);
+this.hasNoSky = false;
 }
+
+@Override
+/** Dimension Name **/
+public String getDimensionName()
+{
+return "Paleocraft";
+}
+
+/** Get Provider for dimension **/
+public static WorldProvider getProviderForDimension(int id)
+{
+return DimensionManager.createProviderFor(fisherman77.paleocraft.common.Paleocraft.PaleocraftDimension);
+}
+
+/** Welcome message **/
+public String getWelcomeMessage()
+{
+return "Entering Paleocraft";
+}
+
+/** What chunk provider to use **/
+public IChunkProvider createChunkGenerator()
+{
+return new ChunkProviderPaleocraft(worldObj, worldObj.getSeed(), true);
+}
+
+/** Can player re-spawn here **/
+public boolean canRespawnHere()
+{
+return false;
+}
+
+/** Set user message **/
+// not sure if this works any more ?
+protected synchronized String setUserMessage(String par1Str)
+{
+return "Building Paleocraft";
+}
+
+/** Determines the dimension the player will be respawned in **/
+public int getRespawnDimension(EntityPlayerMP player)
+{
+return 0;
+}
+
+/** Dimension Movement speed **/
+public double getMovementFactor()
+{
+return 10.0;
+}
+public boolean renderClouds()
+{
+
+ return true;
+
+}
+public String getSaveFolder()
+{
+    return "Paleocraft Dimension";
+}
+public boolean renderStars()
+{
+
+ return true;
+
+}}
