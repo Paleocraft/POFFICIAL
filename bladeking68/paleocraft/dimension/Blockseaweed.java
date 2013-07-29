117c431d775e14707f8d466e0efcf935354ddd2a
Bladeking68 <roborex97@gmail.com>
2013-07-29 11:58:36 +1000

    Update Blockseaweed.java

--- bladeking68/paleocraft/dimension/Blockseaweed.java
+++ bladeking68/paleocraft/dimension/Blockseaweed.java
@@ -1,20 +1,40 @@
 package bladeking68.paleocraft.dimension;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
 
 public class Blockseaweed extends BlockFlower
 {
     public Blockseaweed(int par1)
     {
-        super(par1, Material.plants);
+        super(par1, Material.cactus);
         this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
     }
     public void registerIcons(IconRegister iconRegister)
     {
              blockIcon = iconRegister.registerIcon("paleocraft:seaweed");
     }
+    protected boolean canThisPlantGrowOnThisBlockID(int par1)
+    {
+        return par1 == Block.sand.blockID || par1 == Block.waterStill.blockID || par1 == fisherman77.paleocraft.common.Paleocraft.seaweed.blockID;}
+   
+   
+    @Override
+    public EnumPlantType getPlantType(World world, int x, int y, int z)
+    {
+        return EnumPlantType.Water;
     }
+    public void onEntityCollidedWithBlock(World par1World, int par2, int par3, int par4, Entity par5Entity)
+    {
+        par5Entity.motionX *= 0.4D;
+        par5Entity.motionZ *= 0.4D;
+    }
+}
