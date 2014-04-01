34e1cd5cffaf52436bbf69ce09f8e2dff9c4cf6e
Bladeking68 <roborex97@gmail.com>
2014-04-01 16:32:59 +1100

    Updated?

--- fisherman77/paleocraft/common/mobs/RenderCitipati.java
+++ fisherman77/paleocraft/common/mobs/RenderCitipati.java
@@ -20,10 +20,10 @@ public class RenderCitipati extends RenderLiving
 
 	public void func_177_a(EntityCitipati entityCiti, double d, double d1,
 			double d2, float f, float f1) {
-		super.doRenderLiving(entityCiti, d, d1, d2, f, f1);
+		super.doRender(entityCiti, d, d1, d2, f, f1);
 	}
-
-	public void doRenderLiving(EntityLivingBase entityliving, double d, double d1,
+@Override
+	public void doRender(EntityLivingBase entityliving, double d, double d1,
 			double d2, float f, float f1) {
 		func_177_a((EntityCitipati) entityliving, d, d1, d2, f, f1);
 	}
