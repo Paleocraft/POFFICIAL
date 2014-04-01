e0e1753659f57c7374721c4548eb4eff234d54ea
Bladeking68 <roborex97@gmail.com>
2014-04-01 16:20:49 +1100

    Updated to 1.7

--- fisherman77/paleocraft/common/PaleocraftCommonProxy.java
+++ fisherman77/paleocraft/common/PaleocraftCommonProxy.java
@@ -1,12 +1,16 @@
 package fisherman77.paleocraft.common;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+//import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 //import fisherman77.zeuscraft.common.Zeuscraft;
@@ -40,4 +44,17 @@ public void addRecipes(){ //For adding your Item's recipes
 public void registerRenderers(){
 	
 }
+public void registerSounds() {
+	   
+	 }
+
+public void initCapes()
+{
 }
+/*public void registerEntity(Class<? extends Entity> entityClass, String entityName, int bkEggColor, int fgEggColor) {
+	int id = EntityRegistry.findGlobalUniqueEntityId();
+
+	EntityRegistry.registerGlobalEntityID(entityClass, entityName, id);
+	EntityList.entityEggs.put(Integer.valueOf(id), new EntityEggInfo(id, bkEggColor, fgEggColor));
+}}*/}
+
