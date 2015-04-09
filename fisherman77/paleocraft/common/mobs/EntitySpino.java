191c61cbbbf2875802542cc44308ef30eb6882cd
Bladeking68 <roborex97@gmail.com>
2015-04-09 10:38:04 +1000

    updated to 1.7.10

--- fisherman77/paleocraft/common/mobs/EntitySpino.java
+++ fisherman77/paleocraft/common/mobs/EntitySpino.java
@@ -2,6 +2,9 @@ package fisherman77.paleocraft.common.mobs;
 
 import java.util.Random;
 
+import fisherman77.paleocraft.common.mobs.ai.EntityAISpinoSwim;
+import fisherman77.paleocraft.common.mobs.ai.EntityPaleocraftMob;
+
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
@@ -20,7 +23,7 @@ import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 
-public class EntitySpino extends EntityMob
+public class EntitySpino extends EntityPaleocraftMob
 {
 
 public EntitySpino(World par1World) 
@@ -28,52 +31,52 @@ public EntitySpino(World par1World)
   super(par1World);
   
   this.setSize(this.width * 3.0F, this.height * 3.0F);
-  
+  this.tasks.addTask(4, new EntityAISpinoSwim(this));
   this.tasks.addTask(0, new EntityAISwimming(this));
   this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 0.4D, false));
+  this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityMasso.class, 0.4D, false));
   this.tasks.addTask(3, new EntityAIWander(this, 0.4));
   this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
   this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true)); 
   this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
-  }
+  this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityMasso.class, 0, true));
+ }
 
 	@Override
 	protected void applyEntityAttributes() {
 	    super.applyEntityAttributes();
 	    
 	    getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.8); // moveSpeed
-	    getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(75); // maxHealth
-	    getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(20); //attackStrength
+	    getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(40); // maxHealth
+	    getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(10); //attackStrength
 	}
  
 	 /**
 	  * Returns the sound this mob makes while it's alive.
 	  */
-	 protected String getLivingSound()
-	 {
-	     return "paleocraft:spinoliving";
-	 }
-	
+    protected String getLivingSound()
+    {
+    	 playSound("Paleocraft:mob.spino.spinoliving", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
 	 /**
 	  * Returns the sound this mob makes when it is hurt.
 	  */
-	 protected String getHurtSound()
-	 {
-	     return "paleocraft:spinomad";
-	 }
+    protected String getHurtSound()
+    {
+    	 playSound("Paleocraft:mob.spino.spinomad", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
 	
 	 /**
 	  * Returns the sound this mob makes on death.
 	  */
-	 protected String getDeathSound()
-	 {
-	     return "paleocraft:spinohurt";
-	 }
-	 
-	 public EnumCreatureAttribute getCreatureAttribute()
-	    {
-	        return EnumCreatureAttribute.UNDEAD;
-	    }
+    protected String getDeathSound()
+    {
+    	 playSound("Paleocraft:mob.spino.spinohurt", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
+	
 	
 	protected boolean isAIEnabled()
 	{
