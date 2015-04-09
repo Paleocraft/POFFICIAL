7337a27d342016ce86c9834d82b7a37641738dd5
Bladeking68 <roborex97@gmail.com>
2015-04-09 10:35:31 +1000

    updated to 1.7.10

--- fisherman77/paleocraft/common/mobs/EntityQuetzalcoatlus.java
+++ fisherman77/paleocraft/common/mobs/EntityQuetzalcoatlus.java
@@ -1,17 +1,27 @@
 package fisherman77.paleocraft.common.mobs;
 
+import java.util.Iterator;
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import fisherman77.paleocraft.common.mobs.ai.PaleocraftFlyingLandAI;
+import fisherman77.paleocraft.common.mobs.ai.PaleocraftQuetzLandAI;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityFlying;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityLargeFireball;
+import net.minecraft.entity.projectile.EntitySmallFireball;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.AchievementList;
@@ -19,21 +29,60 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
-public class EntityQuetzalcoatlus extends EntityFlying implements IMob
+public class EntityQuetzalcoatlus extends EntityFlying implements  IMob
 {
     public int courseChangeCooldown;
-    public double waypointX;
+    
+    /** Animation time at previous tick. */
+    public float prevAnimTime;
+    /** Animation time, used to control the speed of the animation cycles (wings flapping, jaw opening, etc.) */
+    public float animTime;public double waypointX;
     public double waypointY;
     public double waypointZ;
-
-    public EntityQuetzalcoatlus(World par1World)  
+    public float field_70886_e;
+    public float destPos;
+    public float field_70884_g;
+    public float field_70888_h;
+    public float field_70889_i = 1.0F;
+    public boolean field_152118_bv;
+    /** The time until the next egg is spawned. */
+    public int timeUntilNextEgg;
+    public double targetX;
+    public double targetY;
+    public double targetZ;
+    private Entity target;
+    private int field_70846_g;
+   // private static final String __OBFID = "CL_00001682";
+    private float heightOffset = 0.5F;
+    /** ticks until heightOffset is randomized */
+    private int heightOffsetUpdateTime;
+    public boolean forceNewTarget;
+    
+    public EntityQuetzPart[] dragonPartArray;
+    /** The head bounding box of a dragon */
+    //public EntityDragonPart dragonPartHead;
+    public EntityQuetzPart dragonPartBody;
+    public EntityQuetzPart dragonPartHead;
+    private Entity targetedEntity;
+    /** Cooldown time between target loss and new target aquirement. */
+    private int aggroCooldown;
+    public int prevAttackCounter;
+    public int attackCounter;
+	private boolean hasAttacked;
+	private int explosionStrength = 1;
+	public EntityQuetzalcoatlus(World par1World)  
     {
      super(par1World);	
-    	//.this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
-    	
-        this.setSize(0.4F, 0.4F);
+    	//this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+   // this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 0.4D, false));
+        this.setSize(2.2F, 2.2F);
+       //this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+      // this.dragonPartArray = new EntityQuetzPart[] {this.dragonPartHead = new EntityQuetzPart(this, "Head", 6.0F, 6.0F)};
+   
+        this.tasks.addTask(3, new PaleocraftQuetzLandAI (this));
     }
     
     @Override
@@ -41,9 +90,13 @@ public class EntityQuetzalcoatlus extends EntityFlying implements IMob
 	    super.applyEntityAttributes();
 	    
 	    getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.4); // moveSpeed
-	    getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(9); // maxHealth
+	    getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(15); // maxHealth
 	}
-
+    
+    
+    /**
+     * Called when the entity is attacked.
+     */
     @SideOnly(Side.CLIENT)
     public boolean func_110182_bF()
     {
@@ -56,10 +109,357 @@ public class EntityQuetzalcoatlus extends EntityFlying implements IMob
         this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
     }
 
-    protected void updateEntityActionState()
+   /* protected void updateEntityActionState()
     {
 
         double d0 = this.waypointX - this.posX;
+        double d1 = this.waypointY - this.posY;
+        double d2 = this.waypointZ - this.posZ;
+        double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+
+        if (d3 < 1.0D || d3 > 3600.0D)
+        {
+            this.waypointX = this.posX + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+            this.waypointY = this.posY + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+            this.waypointZ = this.posZ + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+        }
+
+        if (this.courseChangeCooldown-- <= 0)
+        {
+            this.courseChangeCooldown += this.rand.nextInt(5) + 2;
+            d3 = (double)MathHelper.sqrt_double(d3);
+
+            if (this.isCourseTraversable(this.waypointX, this.waypointY, this.waypointZ, d3))
+            {
+                this.motionX += d0 / d3 * 0.1D;
+                this.motionY += d1 / d3 * 0.1D;
+                this.motionZ += d2 / d3 * 0.1D;
+            }
+            else
+            {
+                this.waypointX = this.posX;
+                this.waypointY = this.posY;
+                this.waypointZ = this.posZ;
+            }
+        }
+
+        double d4 = 64.0D;
+    }
+
+    /**
+     * True if the ghast has an unobstructed line of travel to the waypoint.
+     */
+   /* private boolean isCourseTraversable(double par1, double par3, double par5, double par7)
+    {
+        double d4 = (this.waypointX - this.posX) / par7;
+        double d5 = (this.waypointY - this.posY) / par7;
+        double d6 = (this.waypointZ - this.posZ) / par7;
+        AxisAlignedBB axisalignedbb = this.boundingBox.copy();
+
+        for (int i = 1; (double)i < par7; ++i)
+        {
+            axisalignedbb.offset(d4, d5, d6);
+
+            if (!this.worldObj.getCollidingBoundingBoxes(this, axisalignedbb).isEmpty())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }*/
+
+    
+
+	/*protected void updateEntityActionState()
+    {
+
+        double d0 = this.waypointX - this.posX;
+        double d1 = this.waypointY - this.posY;
+        double d2 = this.waypointZ - this.posZ;
+        double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+
+        if (d3 < 1.0D || d3 > 3600.0D)
+        {
+            this.waypointX = this.posX + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+            this.waypointY = this.posY + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+            this.waypointZ = this.posZ + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
+        }
+
+        if (this.courseChangeCooldown-- <= 0)
+        {
+            this.courseChangeCooldown += this.rand.nextInt(5) + 2;
+            d3 = (double)MathHelper.sqrt_double(d3);
+
+            if (this.isCourseTraversable(this.waypointX, this.waypointY, this.waypointZ, d3))
+            {
+                this.motionX += d0 / d3 * 0.1D;
+                this.motionY += d1 / d3 * 0.1D;
+                this.motionZ += d2 / d3 * 0.1D;
+            }
+            else
+            {
+                this.waypointX = this.posX;
+                this.waypointY = this.posY;
+                this.waypointZ = this.posZ;
+            }
+        }
+
+        double d4 = 64.0D;
+    }
+
+    /**
+     * True if the ghast has an unobstructed line of travel to the waypoint.
+     */
+   /* private boolean isCourseTraversable(double par1, double par3, double par5, double par7)
+    {
+        double d4 = (this.waypointX - this.posX) / par7;
+        double d5 = (this.waypointY - this.posY) / par7;
+        double d6 = (this.waypointZ - this.posZ) / par7;
+        AxisAlignedBB axisalignedbb = this.boundingBox.copy();
+
+        for (int i = 1; (double)i < par7; ++i)
+        {
+            axisalignedbb.offset(d4, d5, d6);
+
+            if (!this.worldObj.getCollidingBoundingBoxes(this, axisalignedbb).isEmpty())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    protected String getLivingSound()
+    {
+    	 playSound("Paleocraft:mob.quetz.Quetz", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
+    
+     * Returns the sound this mob makes when it is hurt.
+     */
+   
+    protected String getLivingSound()
+    {
+    	 playSound("Paleocraft:mob.quetz.Quetz", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
+    
+    protected String getHurtSound()
+    {
+    	 playSound("Paleocraft:mob.quetz.QuetzHurt", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
+
+    /**
+     * Returns the sound this mob makes on death.
+     */
+    protected String getDeathSound()
+    {
+    	 playSound("Paleocraft:mob.other.MedCarnDeath", getSoundVolume(), getSoundPitch());
+    		return null;
+    }
+  
+    protected boolean isAIEnabled()
+	{
+	    return true;
+	}
+    
+    /* private void collideWithEntities(List p_70970_1_)
+    {
+        double d0 = (this.dragonPartBody.boundingBox.minX + this.dragonPartBody.boundingBox.maxX) / 2.0D;
+        double d1 = (this.dragonPartBody.boundingBox.minZ + this.dragonPartBody.boundingBox.maxZ) / 2.0D;
+        Iterator iterator = p_70970_1_.iterator();
+
+        while (iterator.hasNext())
+        {
+            Entity entity = (Entity)iterator.next();
+
+            if (entity instanceof EntityLivingBase)
+            {
+                double d2 = entity.posX - d0;
+                double d3 = entity.posZ - d1;
+                double d4 = d2 * d2 + d3 * d3;
+                entity.addVelocity(d2 / d4 * 4.0D, 0.20000000298023224D, d3 / d4 * 4.0D);
+            }
+        }
+    }
+
+    /**
+     * Attacks all entities inside this list, dealing 5 hearts of damage.
+     */
+  /*  private void attackEntitiesInList(List p_70971_1_)
+    {
+        for (int i = 0; i < p_70971_1_.size(); ++i)
+        {
+            Entity entity = (Entity)p_70971_1_.get(i);
+
+            if (entity instanceof EntityLivingBase)
+            {
+                entity.attackEntityFrom(DamageSource.causeMobDamage(this), 10.0F);
+            }
+        }
+    }
+
+    /**
+     * Sets a new target for the flight AI. It can be a random coordinate or a nearby player.
+     */
+   /* private void setNewTarget()
+    {
+        this.forceNewTarget = false;
+
+        if (this.rand.nextInt(2) == 0 && !this.worldObj.playerEntities.isEmpty())
+        {
+            this.target = (Entity)this.worldObj.playerEntities.get(this.rand.nextInt(this.worldObj.playerEntities.size()));
+        }
+        else
+        {
+            boolean flag = false;
+
+            do
+            {
+                this.targetX = 0.0D;
+                this.targetY = (double)(70.0F + this.rand.nextFloat() * 50.0F);
+                this.targetZ = 0.0D;
+                this.targetX += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
+                this.targetZ += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
+                double d0 = this.posX - this.targetX;
+                double d1 = this.posY - this.targetY;
+                double d2 = this.posZ - this.targetZ;
+                flag = d0 * d0 + d1 * d1 + d2 * d2 > 100.0D;
+            }
+            while (!flag);
+
+            this.target = null;
+        }
+    }
+    public boolean attackEntityFromPart(EntityQuetzPart p_70965_1_, DamageSource p_70965_2_, float p_70965_3_)
+    {
+        if (p_70965_1_ != this.dragonPartHead)
+        {
+            p_70965_3_ = p_70965_3_ / 4.0F + 1.0F;
+        }
+
+        float f1 = this.rotationYaw * (float)Math.PI / 180.0F;
+        float f2 = MathHelper.sin(f1);
+        float f3 = MathHelper.cos(f1);
+        this.targetX = this.posX + (double)(f2 * 5.0F) + (double)((this.rand.nextFloat() - 0.5F) * 2.0F);
+        this.targetY = this.posY + (double)(this.rand.nextFloat() * 3.0F) + 1.0D;
+        this.targetZ = this.posZ - (double)(f3 * 5.0F) + (double)((this.rand.nextFloat() - 0.5F) * 2.0F);
+        this.target = null;
+
+        if (p_70965_2_.getEntity() instanceof EntityPlayer || p_70965_2_.isExplosion())
+        {
+            this.func_82195_e(p_70965_2_, p_70965_3_);
+        }
+
+        return true;
+    }*/
+
+    /**
+     * Called when the entity is attacked.
+     */
+  /*  public boolean attackEntityFrom(DamageSource p_70097_1_, float p_70097_2_)
+    {
+        return false;
+    }
+
+    protected boolean func_82195_e(DamageSource p_82195_1_, float p_82195_2_)
+    {
+        return super.attackEntityFrom(p_82195_1_, p_82195_2_);
+    }*/
+    /*protected boolean isAIEnabled()
+	{
+	    return true;
+	}*/
+	
+	protected boolean canDespawn()
+	{
+	return false;
+	}
+	/*private void setNewTarget()
+    {
+        this.forceNewTarget = false;
+
+        if (this.rand.nextInt(2) == 0 && !this.worldObj.playerEntities.isEmpty())
+        {
+            this.target = (Entity)this.worldObj.playerEntities.get(this.rand.nextInt(this.worldObj.playerEntities.size()));
+        }
+        else
+        {
+            boolean flag = false;
+
+            do
+            {
+                this.targetX = 0.0D;
+                this.targetY = (double)(70.0F + this.rand.nextFloat() * 50.0F);
+                this.targetZ = 0.0D;
+                this.targetX += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
+                this.targetZ += (double)(this.rand.nextFloat() * 120.0F - 60.0F);
+                double d0 = this.posX - this.targetX;
+                double d1 = this.posY - this.targetY;
+                double d2 = this.posZ - this.targetZ;
+                flag = d0 * d0 + d1 * d1 + d2 * d2 > 100.0D;
+            }
+            while (!flag);
+
+            this.target = null;
+        }
+    }
+	public boolean attackEntityFromPart(EntityQuetzPart p_70965_1_, DamageSource p_70965_2_, float p_70965_3_)
+    {
+        if (p_70965_1_ != this.dragonPartHead)
+        {
+            p_70965_3_ = p_70965_3_ / 4.0F + 1.0F;
+        }
+
+        float f1 = this.rotationYaw * (float)Math.PI / 180.0F;
+        float f2 = MathHelper.sin(f1);
+        float f3 = MathHelper.cos(f1);
+        this.targetX = this.posX + (double)(f2 * 5.0F) + (double)((this.rand.nextFloat() - 0.5F) * 2.0F);
+        this.targetY = this.posY + (double)(this.rand.nextFloat() * 3.0F) + 1.0D;
+        this.targetZ = this.posZ - (double)(f3 * 5.0F) + (double)((this.rand.nextFloat() - 0.5F) * 2.0F);
+        this.target = null;
+
+        if (p_70965_2_.getEntity() instanceof EntityPlayer || p_70965_2_.isExplosion())
+        {
+            this.func_82195_e(p_70965_2_, p_70965_3_);
+        }
+
+        return true;
+    }
+
+    /**
+     * Called when the entity is attacked.
+     *//*
+    public boolean attackEntityFrom(DamageSource p_70097_1_, float p_70097_2_)
+    {
+        return false;
+    }
+
+    protected boolean func_82195_e(DamageSource p_82195_1_, float p_82195_2_)
+    {
+        return super.attackEntityFrom(p_82195_1_, p_82195_2_);
+    }*/
+
+	/*@Override
+	public World func_82194_d() {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+	
+	
+	
+	
+	public void updateAITasks()
+    {
+super.updateAITasks();
+      
+int blockX = (int) (posX - 0.5);
+int blockZ = (int) (posZ - 0.5);
+//return posY - worldObj.getHeightValue(blockX, blockZ);
+
+double d0 = this.waypointX - this.posX;
         double d1 = this.waypointY - this.posY;
         double d2 = this.waypointZ - this.posZ;
         double d3 = d0 * d0 + d1 * d1 + d2 * d2;
@@ -115,24 +515,11 @@ public class EntityQuetzalcoatlus extends EntityFlying implements IMob
 
         return true;
     }
-    protected String getLivingSound()
-    {
-        return "paleocraft:Quetz";
+  
+    public double getAltitude() {
+        int blockX = (int) (posX - 0.5);
+        int blockZ = (int) (posZ - 0.5);
+        return posY - worldObj.getHeightValue(blockX, blockZ);
     }
 
-    /**
-     * Returns the sound this mob makes when it is hurt.
-     */
-    protected String getHurtSound()
-    {
-        return "paleocraft:QuetzHurt";
-    }
-
-    /**
-     * Returns the sound this mob makes on death.
-     */
-    protected String getDeathSound()
-    {
-        return "paleocraft:MedCarnDeath";
-    }
 }
