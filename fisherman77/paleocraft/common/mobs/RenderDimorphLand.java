278e5ff070d4b4cff788d98a51eef35e62394eb9
Bladeking68 <roborex97@gmail.com>
2015-04-09 10:55:22 +1000

    Create RenderDimorphLand.java

---
+++ fisherman77/paleocraft/common/mobs/RenderDimorphLand.java
@@ -0,0 +1,49 @@
+package fisherman77.paleocraft.common.mobs;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
+@SideOnly(Side.CLIENT)
+public class RenderDimorphLand extends RenderLiving
+{
+    /** Scale of the model to use */
+    private float scale;
+    
+	private static final ResourceLocation skin = new ResourceLocation("paleocraft", "textures/entity/Dimorph_Land.png");
+
+	public RenderDimorphLand(ModelBase par1ModelBase, float par2, float par3)
+    {
+        super(par1ModelBase, par2 * par3);
+        this.scale = par3;
+    }
+	
+    /**
+     * Applies the scale to the transform matrix
+     */
+    protected void preRenderScale(EntityDimorphodonL par1EntityDimorphodon, float par2)
+    {
+        GL11.glScalef(this.scale, this.scale, this.scale);
+    }
+
+    /**
+     * Allows the render to do any OpenGL state modifications necessary before the model is rendered. Args:
+     * entityLiving, partialTickTime
+     */
+    protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2)
+    {
+        this.preRenderScale((EntityDimorphodonL)par1EntityLivingBase, par2);
+    }
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity entity) {
+		return skin;
+	}
+}
