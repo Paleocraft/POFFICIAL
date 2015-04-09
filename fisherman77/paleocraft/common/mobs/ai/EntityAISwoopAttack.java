f713d90a2b834b07e92815d2844a0d7c8fdf10bd
Bladeking68 <roborex97@gmail.com>
2015-04-09 11:03:24 +1000

    Create EntityAISwoopAttack.java

---
+++ fisherman77/paleocraft/common/mobs/ai/EntityAISwoopAttack.java
@@ -0,0 +1,51 @@
+package fisherman77.paleocraft.common.mobs.ai;
+
+/*import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIBase;
+
+public class EntityAISwoopAttack
+extends EntityAIBase
+{
+private final EntityLiving entityHost;
+private EntityLivingBase attackTarget;
+
+private boolean hasSwooped = false;
+
+private EntityAISwoopAttack class;
+
+public EntityAISwoopAttack(EntityLiving host,Class targClass)
+{
+
+entityHost = host;
+
+class = targClass;
+
+}
+
+public boolean shouldExecute()
+{
+
+//return true if there are no blocks in the way.
+}
+
+public boolean continueExecuting()
+{
+
+//return true if there are no blocks in the way AND !hasSwooped            "!hasSwooped" means that if the entity has already dived in then it should NOT continue the task, reset it and then this task shall be repeated (the entity swoops dwn another time) ONLY IF shouldExecute says so.
+
+}
+
+public void resetTask()
+{
+
+//set everything to null and stuff.
+}
+
+public void updateTask()
+
+{
+
+//Do the actual stuff here, such as making the entity fly towards the location of the attackTarget entity. Then once there, set hasSwooped to true
+
+}}*/
