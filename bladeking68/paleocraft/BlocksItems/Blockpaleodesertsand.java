9aa6f4d03cf8b4648e07464f92dd17290e6e8584
Bladeking68 <roborex97@gmail.com>
2015-04-10 12:51:37 +1000

    updated to 1.7.10

--- bladeking68/paleocraft/BlocksItems/Blockpaleodesertsand.java
+++ bladeking68/paleocraft/BlocksItems/Blockpaleodesertsand.java
@@ -2,130 +2,48 @@ package bladeking68.paleocraft.BlocksItems;
 
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
+//import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.item.EntityFallingSand;
+//import net.minecraft.entity.item.EntityFallingSand;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
-public class Blockpaleodesertsand extends Block
+public class Blockpaleodesertsand extends BlockFalling
 {
 	 public static boolean fallInstantly;
 	
 	
-    public Blockpaleodesertsand(int par1)
+    public Blockpaleodesertsand()
     {
-        super(par1, Material.sand);
-        //this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
+        super(Material.sand);
+     //   this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
+        setHarvestLevel("shovel",0);
     }
-    public void registerIcons(IconRegister iconRegister)
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister p_149651_1_)
     {
-             blockIcon = iconRegister.registerIcon("paleocraft:Paleodesert_sand");
-    }
-    public int idDropped(int par1, Random par2Random, int par3)
+    blockIcon = p_149651_1_.registerIcon("Paleocraft" + ":" + this.getUnlocalizedName().substring(5));
+    
+}
+	@SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
     {
-        return Block.sand.blockID;
+    return blockIcon;
     }
-    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    public Block idDropped(int par1, Random par2Random, int par3)
     {
-        par1World.scheduleBlockUpdate(par2, par3, par4, this.blockID, this.tickRate(par1World));
+        return Blocks.sand;
     }
-
-    /**
-     * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
-     * their own) Args: x, y, z, neighbor blockID
-     */
-    public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5)
-    {
-        par1World.scheduleBlockUpdate(par2, par3, par4, this.blockID, this.tickRate(par1World));
-    }
-
-    /**
-     * Ticks the block if it's been scheduled
-     */
-    public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
-    {
-        if (!par1World.isRemote)
-        {
-            this.tryToFall(par1World, par2, par3, par4);
-        }
-    }
-
-    /**
-     * If there is space to fall below will start this block falling
-     */
-    private void tryToFall(World par1World, int par2, int par3, int par4)
-    {
-        if (canFallBelow(par1World, par2, par3 - 1, par4) && par3 >= 0)
-        {
-            byte b0 = 32;
-
-            if (!fallInstantly && par1World.checkChunksExist(par2 - b0, par3 - b0, par4 - b0, par2 + b0, par3 + b0, par4 + b0))
-            {
-                if (!par1World.isRemote)
-                {
-                    EntityFallingSand entityfallingsand = new EntityFallingSand(par1World, (double)((float)par2 + 0.5F), (double)((float)par3 + 0.5F), (double)((float)par4 + 0.5F), this.blockID, par1World.getBlockMetadata(par2, par3, par4));
-                    this.onStartFalling(entityfallingsand);
-                    par1World.spawnEntityInWorld(entityfallingsand);
-                }
-            }
-            else
-            {
-                par1World.setBlockToAir(par2, par3, par4);
-
-                while (canFallBelow(par1World, par2, par3 - 1, par4) && par3 > 0)
-                {
-                    --par3;
-                }
-
-                if (par3 > 0)
-                {
-                    par1World.setBlock(par2, par3, par4, this.blockID);
-                }
-            }
-        }
-    }
-
-    /**
-     * Called when the falling block entity for this block is created
-     */
-    protected void onStartFalling(EntityFallingSand par1EntityFallingSand) {}
-
-    /**
-     * How many world ticks before ticking
-     */
-    public int tickRate(World par1World)
-    {
-        return 2;
-    }
-
-    /**
-     * Checks to see if the sand can fall into the block below it
-     */
-    public static boolean canFallBelow(World par0World, int par1, int par2, int par3)
-    {
-        int l = par0World.getBlockId(par1, par2, par3);
-
-        if (par0World.isAirBlock(par1, par2, par3))
-        {
-            return true;
-        }
-        else if (l == Block.fire.blockID)
-        {
-            return true;
-        }
-        else
-        {
-            Material material = Block.blocksList[l].blockMaterial;
-            return material == Material.water ? true : material == Material.lava;
-        }
-    }
-
-    /**
-     * Called when the falling block entity for this block hits the ground and turns back into a block
-     */
-    public void onFinishFalling(World par1World, int par2, int par3, int par4, int par5) {}
 }
