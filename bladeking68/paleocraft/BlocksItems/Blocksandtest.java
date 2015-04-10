d85e578f3f112e53f214f5fa331e3fa88d2170c2
Bladeking68 <roborex97@gmail.com>
2015-04-10 12:53:29 +1000

    updated to 1.7.10

--- bladeking68/paleocraft/BlocksItems/Blocksandtest.java
+++ bladeking68/paleocraft/BlocksItems/Blocksandtest.java
@@ -3,17 +3,18 @@ package bladeking68.paleocraft.BlocksItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
+//import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class Blocksandtest extends Block
 {
     public Blocksandtest(int par1)
     {
-        super(par1, Material.sand);
+        super(Material.sand);
        // this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
     }
-    public void registerIcons(IconRegister iconRegister)
+    public void registerIcons(IIconRegister iconRegister)
     {
              blockIcon = iconRegister.registerIcon("paleocraft:Test1");
     }
