3e1bc2317c364d1a4c41132cfc10748c584e88f6
Bladeking68 <roborex97@gmail.com>
2015-04-10 13:00:16 +1000

    Create ItemNestBabyAwake.java

---
+++ bladeking68/paleocraft/BlocksItems/ItemNestBabyAwake.java
@@ -0,0 +1,55 @@
+package bladeking68.paleocraft.BlocksItems;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+//import net.minecraft.client.renderer.texture.IconRegister;
+import java.util.Iterator;
+import java.util.List;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+//import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+//import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+//import net.minecraft.entity.EntityLivingData;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+//import net.minecraft.util.EnumMovingObjectType;
+import net.minecraft.util.Facing;
+import net.minecraft.util.ChatComponentTranslation;
+//import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import fisherman77.paleocraft.common.mobs.EntityDromaeosaurus;
+import fisherman77.paleocraft.common.mobs.EntityTroodon;
+
+public class ItemNestBabyAwake extends Item
+{
+       private Object iconIndex;
+
+       public ItemNestBabyAwake()
+       {
+             
+            this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
+       }
+      
+
+   
+}
