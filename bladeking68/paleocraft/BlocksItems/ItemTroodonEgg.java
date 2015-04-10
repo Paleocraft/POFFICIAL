f0e1db59d9ec0dc56dea99fbfb193ebf70056c67
Bladeking68 <roborex97@gmail.com>
2015-04-10 13:00:38 +1000

    updated to 1.7.10

--- bladeking68/paleocraft/BlocksItems/ItemTroodonEgg.java
+++ bladeking68/paleocraft/BlocksItems/ItemTroodonEgg.java
@@ -3,6 +3,7 @@ package bladeking68.paleocraft.BlocksItems;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
@@ -11,24 +12,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.client.renderer.texture.IconRegister;
+//import net.minecraft.client.renderer.texture.IconRegister;
 import java.util.Iterator;
 import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
+//import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityEggInfo;
+//import net.minecraft.entity.EntityEggInfo;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.EntityLivingData;
+//import net.minecraft.entity.EntityLivingData;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumMovingObjectType;
+//import net.minecraft.util.EnumMovingObjectType;
 import net.minecraft.util.Facing;
-import net.minecraft.util.Icon;
+//import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
@@ -41,9 +42,9 @@ public class ItemTroodonEgg extends Item
 {
        private Object iconIndex;
 
-       public ItemTroodonEgg(int id)
+       public ItemTroodonEgg()
        {
-             super(id);
+            
              //this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
        }
       
@@ -65,7 +66,7 @@ public class ItemTroodonEgg extends Item
        }
        @Override
        @SideOnly(Side.CLIENT)
-       public void registerIcons(IconRegister iconRegister)
+       public void registerIcons(IIconRegister iconRegister)
        {
        itemIcon = iconRegister.registerIcon("paleocraft:" + "DinoEgg");
        }
