0d77076d38f2aa37bc3ae541b77ad5964410d84b
Bladeking68 <roborex97@gmail.com>
2015-04-10 12:52:33 +1000

    updated to 1.7.10

--- bladeking68/paleocraft/BlocksItems/Blockpaleoplainsdirt.java
+++ bladeking68/paleocraft/BlocksItems/Blockpaleoplainsdirt.java
@@ -2,24 +2,39 @@ package bladeking68.paleocraft.BlocksItems;
 
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
+//import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.IIcon;
 
 public class Blockpaleoplainsdirt extends Block
 {
-    public Blockpaleoplainsdirt(int par1)
+    public Blockpaleoplainsdirt()
     {
-        super(par1, Material.ground);
+        super(Material.ground);
         //this.setCreativeTab(fisherman77.paleocraft.common.Paleocraft.PaleocraftBlocks);
     }
-    public void registerIcons(IconRegister iconRegister)
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister p_149651_1_)
     {
-             blockIcon = iconRegister.registerIcon("paleocraft:Paleoplains_dirt");
+    blockIcon = p_149651_1_.registerIcon("Paleocraft" + ":" + this.getUnlocalizedName().substring(5));
+    
+}
+	@SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
+    {
+    return blockIcon;
     }
-    public int idDropped(int par1, Random par2Random, int par3)
+    public Block idDropped(int par1, Random par2Random, int par3)
     {
-        return Block.dirt.blockID;
+        return Blocks.dirt;
     }}
